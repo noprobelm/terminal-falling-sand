a4f38431714af339a60c289b367ab91df278cbc5
noprobelm <noprobelm@protonmail.com>
2023-12-09 11:28:19 -0500

    Renamed 'GridList' to 'Grid'

--- terminal_falling_sand/__main__.py
+++ terminal_falling_sand/__main__.py
@@ -9,10 +9,12 @@ def main():
     x_max = console.width
     y_max = console.height * 2
     grid = GridList(x_max, y_max)
-    grid.fill_random(2, range(x_max // 4, int(x_max * 0.75)), range(y_max // 2))
+    grid.fill_random(2, range(x_max // 4, int(x_max * 0.75)), range(y_max))
+    refresh_rate = 1 / 60
 
     with Live(grid, console=console, screen=True, auto_refresh=False) as live:
         while True:
+            sleep(refresh_rate)
             grid.step()
             live.update(grid, refresh=True)
 

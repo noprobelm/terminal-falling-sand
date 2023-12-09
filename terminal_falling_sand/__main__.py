fa040e591c3f7fdc3c5a40f001c9ee8123093d15
noprobelm <noprobelm@protonmail.com>
2023-12-09 11:41:44 -0500

    Cleaned up main to make setting options easier

--- terminal_falling_sand/__main__.py
+++ terminal_falling_sand/__main__.py
@@ -1,22 +1,34 @@
-from .grid import GridList
+from .grid import Grid
 from rich.console import Console
 from rich.live import Live
 from time import sleep
+from typing import Optional
+
+
+def terminal_render(grid: Grid, refresh_per_second: Optional[int] = None):
+    if refresh_per_second is not None:
+        refresh_rate = 1 / refresh_per_second
+    else:
+        refresh_rate = 0
+    with Live(grid, screen=True, auto_refresh=False) as live:
+        while True:
+            grid.step()
+            live.update(grid, refresh=True)
+            sleep(refresh_rate)
+
+
+def build_grid(xmax: int, ymax: int) -> Grid:
+    return Grid(xmax, ymax)
 
 
 def main():
     console = Console()
-    x_max = console.width
-    y_max = console.height * 2
-    grid = GridList(x_max, y_max)
-    grid.fill_random(2, range(x_max // 4, int(x_max * 0.75)), range(y_max))
-    refresh_rate = 1 / 60
+    xmax = console.width
+    ymax = console.height * 2
+    grid = build_grid(console.width, console.height * 2)
+    grid.fill_random(2, range(xmax // 4, int(xmax * 0.75)), range(ymax))
 
-    with Live(grid, console=console, screen=True, auto_refresh=False) as live:
-        while True:
-            sleep(refresh_rate)
-            grid.step()
-            live.update(grid, refresh=True)
+    terminal_render(grid, 60)
 
 
 if __name__ == "__main__":

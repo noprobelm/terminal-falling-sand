36e101de63d9346179989566320c20623a6763dc
noprobelm <noprobelm@protonmail.com>
2023-12-10 16:36:57 -0500

    Added max_coord ivar for Matrix

--- terminal_falling_sand/matrix.py
+++ terminal_falling_sand/matrix.py
@@ -1,6 +1,6 @@
 import random
 from .cell import Cell, Coordinate
-from .state import Empty, MovableSolid
+from .cell_state import Empty, MovableSolid
 from rich.console import Console, ConsoleOptions
 from rich.style import Style
 from rich.segment import Segment
@@ -9,9 +9,8 @@ from rich.segment import Segment
 class CellMatrix(list):
     def __init__(self, xmax: int, ymax: int) -> None:
         grid = []
-        self.xmax = xmax - 1
-        self.ymax = ymax - 1
-        self.midpoint = self.xmax // 2
+        self.max_coord = Coordinate(xmax - 1, ymax - 1)
+        self.midpoint = self.max_coord.x // 2
         if self.midpoint % 2 == 1:
             self.midpoint += 1
 
@@ -19,7 +18,7 @@ class CellMatrix(list):
             grid.append([])
             for x in range(xmax):
                 c = Coordinate(x, y)
-                e = Cell(c, Empty(), self.xmax, self.ymax)
+                e = Cell(c, self.max_coord, Empty())
                 grid[c.y].append(e)
         super().__init__(grid)
 
@@ -28,22 +27,19 @@ class CellMatrix(list):
             for y in ry:
                 if random.randint(1, chance) == chance:
                     c = Coordinate(x, y)
-                    e = Cell(c, MovableSolid(), self.xmax, self.ymax)
+                    e = Cell(c, self.max_coord, MovableSolid())
                     self[c.y][c.x] = e
 
     def step(self):
-        midpoint = self.xmax // 2
-        if midpoint % 2 == 1:
-            midpoint += 1
-        for y in range(self.ymax + 1):
-            for x1 in range(0, midpoint + 1):
-                self[self.ymax - y][x1].step(self)
-            for x2 in range(midpoint, self.xmax + 1):
-                self[self.ymax - y][self.xmax - x2 - midpoint].step(self)
+        for y in range(self.max_coord.y + 1):
+            for x1 in range(0, self.midpoint + 1):
+                self[self.max_coord.y - y][x1].step(self)
+            for x2 in range(self.midpoint, self.max_coord.x + 1):
+                self[self.max_coord.y - y][self.max_coord.x - x2 - self.midpoint].step(self)
 
     def __rich_console__(self, console: Console, options: ConsoleOptions):
-        for y in range(self.ymax)[::2]:
-            for x in range(self.xmax + 1):
+        for y in range(self.max_coord.y)[::2]:
+            for x in range(self.max_coord.x + 1):
                 bg = self[y][x].state._color
                 fg = self[y + 1][x].state._color
                 yield Segment("▄", Style(color=fg, bgcolor=bg))

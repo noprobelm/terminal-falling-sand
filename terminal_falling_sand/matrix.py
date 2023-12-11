356edd9d6429d0c6f207d037868bb5296422f88b
noprobelm <noprobelm@protonmail.com>
2023-12-11 10:32:01 -0500

    Documented rich console protocol method

--- terminal_falling_sand/matrix.py
+++ terminal_falling_sand/matrix.py
@@ -2,7 +2,7 @@
 
 from typing import Type
 
-from rich.console import Console, ConsoleOptions
+from rich.console import Console, ConsoleOptions, RenderResult
 from rich.segment import Segment
 from rich.style import Style
 
@@ -59,6 +59,20 @@ class CellMatrix(list):
         self[y][x] = element(coord, self.max_coord)
 
     def step(self) -> None:
+        """Steps the simulation forward once
+
+        Explores every element in the simulation by working bottom to top, then middle to left/right for each row. Each
+        step in the simulation calls the 'step' method on the underlying Cell type. Cell.step will determien its next
+        place in the CellMatrix and modify the CellMatrix reference passed to it accordingly.
+
+        Issue:
+            When we step each element from left -> right or right -> left, the elements on the trailing end exhibit odd
+            behavior. Specifically, elements will move diagonally and to the left (or right) depending on the order
+            we're stepping them in. Unsure of the exact cause of this, but for now, working "middle out" in either
+            direction visually solves the problem. This probably means there's some odd behavior in the middle of the
+            matrix for each step, but it's not visually identifiable. Working "middle out" is an acceptable workaround
+            for now.
+        """
         for y in range(self.max_coord.y + 1):
             for x1 in range(0, self.midpoint + 1):
                 self[self.max_coord.y - y][x1].step(self)
@@ -67,7 +81,18 @@ class CellMatrix(list):
                     self
                 )
 
-    def __rich_console__(self, console: Console, options: ConsoleOptions):
+    def __rich_console__(
+        self, console: Console, options: ConsoleOptions
+    ) -> RenderResult:
+        """Renders each Cell in the simulation using the Rich Console Protocol
+
+        Due to the typical 2:1 height/width aspect ratio of a terminal, each cell rendered from the CellMatrix simulation
+        actually occupies 2 rows in the terminal. I picked up this trick from rich's __main__ module. Run
+        'python -m rich and observe the color palette at the top of stdout for another example of what this refers to.
+
+        Yields:
+            2 cells in the simulation, row by row, until all cell states have been rendered.
+        """
         for y in range(self.max_coord.y)[::2]:
             for x in range(self.max_coord.x + 1):
                 bg = self[y][x].color

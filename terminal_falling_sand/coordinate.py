5eebccc5ee2f68dd646ad5ea2cf77d7574df16d7
noprobelm <noprobelm@protonmail.com>
2023-12-11 20:55:36 -0500

    Replaced tuples with Coordinates

--- terminal_falling_sand/coordinate.py
+++ terminal_falling_sand/coordinate.py
@@ -10,19 +10,6 @@ from dataclasses import dataclass
 from enum import Enum
 
 
-class MooreNeighborhood(Enum):
-    """Enumeration for variants of neighbors found in a Moore Neighborhood"""
-
-    UPPER_LEFT = (-1, -1)
-    UPPER = (0, -1)
-    UPPER_RIGHT = (1, -1)
-    RIGHT = (1, 0)
-    LOWER_RIGHT = (1, 1)
-    LOWER = (0, 1)
-    LOWER_LEFT = (-1, 1)
-    LEFT = (-1, 0)
-
-
 @dataclass(eq=True, order=True, frozen=True)
 class Coordinate:
     """An x/y coordinate to reference location in a CellMatrix"""
@@ -33,6 +20,19 @@ class Coordinate:
     def __add__(self, other: Coordinate) -> Coordinate:
         """Returns the sum of one coordinate and another. Primarily used to identify neighbors"""
         return Coordinate(self.x + other.x, self.y + other.y)
+
+
+class MooreNeighborhood(Enum):
+    """Enumeration for variants of neighbors found in a Moore Neighborhood"""
+
+    UPPER_LEFT = Coordinate(-1, -1)
+    UPPER = Coordinate(0, -1)
+    UPPER_RIGHT = Coordinate(1, -1)
+    RIGHT = Coordinate(1, 0)
+    LOWER_RIGHT = Coordinate(1, 1)
+    LOWER = Coordinate(0, 1)
+    LOWER_LEFT = Coordinate(-1, 1)
+    LEFT = Coordinate(-1, 0)
 
 
 Neighbors = namedtuple("Neighbors", [member.name for member in MooreNeighborhood])

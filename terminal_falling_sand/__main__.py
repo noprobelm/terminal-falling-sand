4f644d900f41b53fc0af6bcd0043689c5d6ba181
noprobelm <noprobelm@protonmail.com>
2023-12-11 20:13:07 -0500

    Renamed 'build_grid' to 'build_matrix'

--- terminal_falling_sand/__main__.py
+++ terminal_falling_sand/__main__.py
@@ -43,7 +43,7 @@ def simulate(
             sleep(refresh_rate)
 
 
-def build_grid(xmax: int, ymax: int) -> CellMatrix:
+def build_matrix(xmax: int, ymax: int) -> CellMatrix:
     """Builds a grid based on x and y constraints
 
     Args:

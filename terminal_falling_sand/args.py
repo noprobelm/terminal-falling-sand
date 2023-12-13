908aea903e67023d0012b72ed2dd690278e1fce1
noprobelm <noprobelm@protonmail.com>
2023-12-13 12:28:48 -0500

    Reordered args

--- terminal_falling_sand/args.py
+++ terminal_falling_sand/args.py
@@ -5,6 +5,22 @@ import argparse
 parser = argparse.ArgumentParser(description="Example argparse script")
 
 parser.add_argument(
+    "-r",
+    "--refresh-rate",
+    type=int,
+    default=60,
+    help="The refresh rate of the simulation",
+)
+
+parser.add_argument(
+    "-d",
+    "--duration",
+    type=int,
+    default=0,
+    help="The duration the simulation should run for",
+)
+
+parser.add_argument(
     "-x",
     "--debug",
     action="store_true",
@@ -21,20 +37,5 @@ parser.add_argument(
     help="Disables simulation rendering to the terminal",
 )
 
-parser.add_argument(
-    "-r",
-    "--refresh-rate",
-    type=int,
-    default=60,
-    help="The refresh rate of the simulation",
-)
-
-parser.add_argument(
-    "-d",
-    "--duration",
-    type=int,
-    default=0,
-    help="The duration the simulation should run for",
-)
 
 args = vars(parser.parse_args())

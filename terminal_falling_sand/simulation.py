3c31ceb2a6083844180e3d9682986cd17fe1f738
noprobelm <noprobelm@protonmail.com>
2023-12-13 11:28:07 -0500

    Documented the Simulation class

--- terminal_falling_sand/simulation.py
+++ terminal_falling_sand/simulation.py
@@ -11,7 +11,18 @@ from .matrix import CellMatrix
 
 
 class Simulation:
-    def __init__(self):
+    """A class to run a simulation from the terminal
+
+    Default behavior is to run the simulation at the current dimensions of the terminal. Currently, this is not
+    modifiable by the user without direct interference with the underlying matrix attr
+
+    Attributes:
+        1. matrix (CellMatrix): The underlying cell matrix
+    """
+
+    def __init__(self) -> None:
+        """Initializes an instance of the Simulation class"""
+
         console = Console()
         xmax = console.width
         ymax = console.height * 2
@@ -37,16 +48,23 @@ class Simulation:
 
     def start(
         self,
+        refresh_rate: Optional[int] = None,
         duration: Union[float, int] = 0,
-        fps: int = 60,
         render: Optional[bool] = True,
         debug=False,
-    ):
-        if fps > 0:
-            refresh_rate = 1 / fps
-            print(refresh_rate)
+    ) -> None:
+        """Sets initial parameters for the simluation, then runs it
+
+        Args:
+            duration (Union[float, int]): The duration the simulation should run for. Defaults to 0 (infinity)
+            refresh_rate (int): The number of times the simluation should run before sleeping. Defaults to None
+            render (bool): Controls if the simulation renders to the terminal. Defaults to True
+            debug (bool): Controls if the simulation runs in debug mode. This will run cProfile and disable rendering
+        """
+        if refresh_rate is None:
+            sleep_time = 0
         else:
-            refresh_rate = fps
+            sleep_time = 1 / refresh_rate
 
         if duration == 0:
             duration = float("inf")
@@ -59,27 +77,34 @@ class Simulation:
             )
 
         elif render is True:
-            self.run(duration, refresh_rate, True)
+            self.run(duration, sleep_time, True)
 
         else:
-            self.run(duration, refresh_rate, False)
+            self.run(duration, sleep_time, False)
 
     def run(
         self,
         duration: Union[float, int],
-        refresh_rate: Union[float, int],
+        sleep_time: Union[float, int],
         render: bool,
-    ):
+    ) -> None:
+        """Runs the simulation
+
+        Args:
+            duration (Union[float, int]): The duration the simulation should run for
+            sleep_time (Union[float, int]): The time the simulation should sleep between each step
+            render: bool: Cotnrols if the simulation renders to the terminal
+        """
         elapsed = 0
         if render is True:
             with Live(self.matrix, screen=True, auto_refresh=False) as live:
                 while elapsed < duration:
                     self.matrix.step()
                     live.update(self.matrix, refresh=True)
-                    sleep(refresh_rate)
+                    sleep(sleep_time)
                     elapsed += 1
         else:
             while elapsed < duration:
                 self.matrix.step()
-                sleep(refresh_rate)
+                sleep(sleep_time)
                 elapsed += 1

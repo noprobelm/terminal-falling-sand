e7886e86ea1db2eb68bc836832bf81bcf67ddbcf
noprobelm <noprobelm@protonmail.com>
2023-12-13 12:18:42 -0500

    Added args to main

--- terminal_falling_sand/__main__.py
+++ terminal_falling_sand/__main__.py
@@ -2,12 +2,13 @@
 
 from .simulation import Simulation
 from .scenarios import SCENARIO_1
+from .args import args
 
 
 def main() -> None:
     """Main entrypoint for running a simulation on default settings"""
     sim = Simulation.from_matrix(SCENARIO_1)
-    sim.start(60)
+    sim.start(**args)
 
 
 if __name__ == "__main__":

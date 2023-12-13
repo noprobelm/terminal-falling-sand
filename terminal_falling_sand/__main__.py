2a653ef15d055fad9ee0ad550e5c4aeda364ae2c
noprobelm <noprobelm@protonmail.com>
2023-12-13 11:14:43 -0500

    Removed unused imports and methods

--- terminal_falling_sand/__main__.py
+++ terminal_falling_sand/__main__.py
@@ -1,43 +1,10 @@
 """Main entrypoint for running the falling sand simulation"""
 
-import random
-from time import sleep
-from typing import Optional
 from .simulation import Simulation
-from rich.console import Console
-from rich.live import Live
-
-from .coordinate import Coordinate
-from .elements import Sand, Water
-from .matrix import CellMatrix
-
-
-def build_matrix(xmax: int, ymax: int) -> CellMatrix:
-    """Builds a matrix based on x and y constraints
-
-    Args:
-        xmax (int): The maximum x value for the matrix
-        ymax (int): The maximum y value for the matrix
-
-    Note
-        If rendering to a terminal, ymax might be best to set to twice the terminal's height. This is to accommodate
-        the height/width aspect ratio of ASCII text in the terminal (which is usually 2:1).
-
-    Returns:
-        CellMatrix
-
-    """
-    return CellMatrix(xmax, ymax)
 
 
 def main() -> None:
-    """Main entrypoint for setting matrix parameters and spawning elements into the simulation
-
-    - Sets an xmax/ymax based on the invoking terminal's dimensions, then builds a matrix based on these values.
-    - Spawns in various elements to participate in the simulation
-    - Runs the simulate method using default args
-
-    """
+    """Main entrypoint for running a simulation on default settings"""
     sim = Simulation()
     sim.start(render=True)
 

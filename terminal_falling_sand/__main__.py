9926e93e523cc99dabf0b818829eea9ac72324da
noprobelm <noprobelm@protonmail.com>
2023-12-13 13:20:25 -0500

    Import scenario module instead of scenarios

--- terminal_falling_sand/__main__.py
+++ terminal_falling_sand/__main__.py
@@ -1,13 +1,13 @@
 """Main entrypoint for running the falling sand simulation"""
 
 from .simulation import Simulation
-from .scenarios import SCENARIO_1
+from . import scenarios
 from .args import args
 
 
 def main() -> None:
     """Main entrypoint for running a simulation on default settings"""
-    sim = Simulation.from_matrix(SCENARIO_1)
+    sim = Simulation.from_matrix(scenarios.SCENARIO_1)
     sim.start(**args)
 
 
